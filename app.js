2b67160ffddc24b90f2b7b630b3d2fe755113d88
Malinga Pasindu <malingapasindu14@gmail.com>
2024-09-13 08:50:23 +0530

    5th Commit

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
 
-function load(){
-    let search = document.getElementById("txtSearch").value;
-    fetch (`https://api.weatherapi.com/v1/forecast.json?key=00c32db79d684476a03182846240909&q=${search}&days=7`)
-    .then(res=>res.json())
-    .then(data=>{
-        console.log(search);
-        console.log("Hello My Friends...");
-    })
-    
+function load() {
+    fetch(`https://api.weatherapi.com/v1/forecast.json?key=00c32db79d684476a03182846240909&q=${document.getElementById("txtSearch").value}&days=7`)
+        .then(res => res.json())
+        .then(data => {
+            document.getElementById("titleCity").innerHTML= "City";
+            document.getElementById("titleCountry").innerHTML="Country";
+            document.getElementById("titleTempC").innerHTML="Temp°C";
+            document.getElementById("titleTempF").innerHTML="Temp°F";
+
+            document.getElementById("city").innerHTML = data.location.name;
+            document.getElementById("country").innerHTML = data.location.country;
+            document.getElementById("tempC").innerHTML = data.current.temp_c+"°C";
+            document.getElementById("tempF").innerHTML = data.current.temp_f+"°F";
+        })
 }
+
+
 
