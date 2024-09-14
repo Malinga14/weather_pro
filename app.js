757bb3728d4d32322396a632daff72fc1d85aa6f
Malinga Pasindu <malingapasindu14@gmail.com>
2024-09-15 01:55:43 +0530

    4th commit

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-
-function load() {
+load();
+function search() {
     fetch(`https://api.weatherapi.com/v1/forecast.json?key=00c32db79d684476a03182846240909&q=${document.getElementById("txtSearch").value}&days=7`)
         .then(res => res.json())
         .then(data => {
@@ -13,6 +13,30 @@ function load() {
             document.getElementById("tempC").innerHTML = data.current.temp_c+"°C";
             document.getElementById("tempF").innerHTML = data.current.temp_f+"°F";
         })
+}
+
+function load(){
+    fetch(`https://api.weatherapi.com/v1/forecast.json?key=00c32db79d684476a03182846240909&q=$london&days=7`)
+        .then(res => res.json())
+        .then(data => {
+            document.getElementById("tempUk").innerHTML = "Temp : "+data.current.temp_c+"°C / "+data.current.temp_f+"°F";
+            document.getElementById("windUk").innerHTML ="Wind : "+data.current.wind_kph+" kmph / "+data.current.wind_mph+" mph";
+        })
+
+        fetch(`https://api.weatherapi.com/v1/forecast.json?key=00c32db79d684476a03182846240909&q=$New York&days=7`)
+        .then(res => res.json())
+        .then(data => {
+            document.getElementById("tempUsa").innerHTML = "Temp : "+data.current.temp_c+"°C / "+data.current.temp_f+"°F";
+            document.getElementById("windUsa").innerHTML ="Wind : "+data.current.wind_kph+" kmph / "+data.current.wind_mph+" mph";
+        })
+
+        fetch(`https://api.weatherapi.com/v1/forecast.json?key=00c32db79d684476a03182846240909&q=$sydney&days=7`)
+        .then(res => res.json())
+        .then(data => {
+            document.getElementById("tempAus").innerHTML = "Temp : "+data.current.temp_c+"°C / "+data.current.temp_f+"°F";
+            document.getElementById("windAus").innerHTML ="Wind : "+data.current.wind_kph+" kmph / "+data.current.wind_mph+" mph";
+        })
+
 }
 
 
